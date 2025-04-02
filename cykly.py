296df578502c936f960a2d082d2d61fc79f31a9f
Meliss <fryingpan.1369@gmail.com>
2025-04-02 15:10:32 +0200

    Add files via upload

--- cykly.py
+++ cykly.py
@@ -1,15 +1,17 @@
-import random                       #3*100 = 30 - ondra matyáš, 2.4. 2025
-#im thinking miku miku ooeeeoo im thinking miku miku ooeeoo im thinking miku miku ooeeoo im thinking miku miku ooeeoo im on top of the world because of you
+import random
+
 for x in range(30): 
     a = random.randint(0,10)  
     b = random.randint(0,10) 
-    znamenko = random.randint(1,2)
+    znamenko = random.randint(1,4)
 
     if znamenko == 1:
         print(f"{a} * {b} = ")    
-    else:   
+    elif znamenko == 2:   
         print(f"{a} / {b} = ")
+    elif znamenko == 3: 
         print(f"{a} + {b} = ")
+    elif znamenko == 4: 
         print(f"{a} - {b} = ")
     
     
